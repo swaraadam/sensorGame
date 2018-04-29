8e860ab7b78fc24f4c0cf29cf6d3fce911cf4bfe
swaraadam <bimaswaraadam@gmail.com>
2018-04-29 09:12:13 +0700

    Init
    Sensor test (include gyrospe and proximity)

---
+++ Assets/Script/MainSensor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSensor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        gyroInput();
+	}
+
+    void gyroInput()
+    {
+        transform.rotation = playerPos(Input.gyro.attitude);
+    }
+
+    private static Quaternion playerPos(Quaternion a)
+    {
+        return new Quaternion(a.x, a.y, a.z, a.w);
+    }
+}
