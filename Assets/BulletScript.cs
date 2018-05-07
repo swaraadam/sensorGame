309d3c0183ee33b8909d8781f8615ae8da6b5a1b
swaraadam <bimaswaraadam@gmail.com>
2018-05-07 19:46:40 +0700

    Add Proxiity

---
+++ Assets/BulletScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float bulletSpeed = 1.5f;
+    public Rigidbody bullet;
+    private Gyroscope gyroscope;
+    private bool isGyro = false;
+
+    private void Start()
+    {
+        isGyro = gyroTest();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (PAProximity.proximity == PAProximity.Proximity.NEAR)
+        {
+            tembak();
+        }
+        if (isGyro)
+        {
+            transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        }
+    }
+
+    void tembak()
+    {
+        Rigidbody bulletCreate = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletCreate.velocity = transform.forward * bulletSpeed;
+    }
+
+    private bool gyroTest()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+
+            return true;
+        }
+        return false;
+    }
+}
---
+++ Assets/TestSensor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSensor : MonoBehaviour {
+
+
+    private Gyroscope gyroscope;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (gyroTest())
+        {
+            transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        }
+	}
+
+    private bool gyroTest()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+
+            return true;
+        }
+        return false;
+    }
+}
