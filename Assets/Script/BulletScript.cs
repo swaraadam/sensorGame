528bfa725d2d729a90ff82d9cbd829aa10af3fae
swaraadam <bimaswaraadam@gmail.com>
2018-05-08 08:01:31 +0700

    Add Compass and Player Char
    Add Compass in top screen
    Change player Character
    Chang bullet speed
    Scale Bullet
    Pose Char

---
+++ Assets/Script/BulletScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+    public float bulletSpeed = 9f;
+    public Rigidbody bullet;
+    private Gyroscope gyroscope;
+    private bool isGyro = false;
+
+    private void Start()
+    {
+        isGyro = gyroTest();
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+
+        if (PAProximity.proximity == PAProximity.Proximity.NEAR||Input.GetKeyDown(KeyCode.Space))
+        {
+            tembak();
+        }
+        if (isGyro)
+        {
+            transform.Rotate(0, -Input.gyro.rotationRateUnbiased.y, 0);
+        }
+    }
+
+    void tembak()
+    {
+        Rigidbody bulletCreate = (Rigidbody)Instantiate(bullet, transform.position, transform.rotation);
+        bulletCreate.velocity = transform.forward * bulletSpeed;
+        Destroy(bulletCreate.gameObject, 2f);
+    }
+
+    private bool gyroTest()
+    {
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyroscope = Input.gyro;
+            gyroscope.enabled = true;
+
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ Assets/Script/Compass.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Compass : MonoBehaviour {
+
+    public RawImage rawImage;
+    public Transform player;
+	// Update is called once per frame
+	void Update () {
+        rawImage.uvRect = new Rect(player.localEulerAngles.y / 360, 0, 1, 1);
+	}
+}
